dfd830bdbfe1cee4cfb060ed51e1c359154827f4
xiaodao <lychees67@gmail.com>
2019-09-19 21:30:37 +0800

    Add login interface

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -2,6 +2,15 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
+    <button class="btn-base bg-purple" @click="walletLogin('GitHub')">
+      <span>Github 登录</span>
+    </button>
+    <button class="btn-base bg-purple" @click="walletLogin('Twitter')">
+      <span>Twitter 登录</span>
+    </button>
+    <button class="btn-base bg-purple" @click="walletLogin('Metamask')">
+      <span>Metamask 登录</span>
+    </button>
   </div>
 </template>
 
