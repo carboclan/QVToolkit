00f030e44cb2f1b650d57589dfde9a350433adcf
Kumo Lam <gdut7890@outlook.com>
2019-09-20 22:17:18 +0800

    后端验证GitHub code

--- handler.js
+++ handler.js
@@ -1,107 +1,128 @@
 //
 
-var http = require('http');
-var url = require('url');
+// var http = require('http');
+// var url = require('url');
+var axios = require('axios');
+var app = require('express')();
 var querystring = require('querystring');
-var oauth = require('./oauth.js');
-var token = require('./token.js');
+var http = require('http').Server(app);
+// var oauth = require('./oauth.js');
+// var token = require('./token.js');
 var config = require('./config');
 
-var server = http.createServer(function (req, res) {
-    const pureURL = url.parse(req.url);
-    console.log(pureURL.pathname);
-    var queryData = {};
-    if ('/' === pureURL.pathname){
-        res.statusCode = 200;
-        res.write('<p><a href="/login/twitter">Login with Twitter</a></p>' +
-                  '<p><a href="/login/github">Login with GitHub</a></p>');
-        return res.end();
-    }
-    else if ('/login/twitter' === pureURL.pathname) {
-        oauth.getToken(function (jsonToken) {
-            res.writeHead(301, {'Location': 'https://api.twitter.com/oauth/authenticate?oauth_token='+jsonToken.oauth_token});
-            // res.statusCode = 200;
-            // res.write(jsonToken.oauth_token);
-            return res.end();
-        })
-    }
-    else if ('/login/github' === pureURL.pathname) {
-        token.generateRandomString(function (scope) {
-            res.writeHead(301, {'Location':
-                    'https://github.com/login/oauth/authorize?client_id=' + config.GITHUB_CLIENT_ID +
-                    '&scope=read:public_repo,read:user,user:email' +
-                    '&state=' + scope});
-            return res.end();
-        })
-
-    }
-    else if ('/complete/twitter' === pureURL.pathname) {
-        queryData = querystring.parse(pureURL.query);
-        console.log(queryData);
-        oauth.getAccessToken(queryData.oauth_token, queryData.oauth_verifier, function(jsonToken) {
-            console.log(jsonToken);
-            res.statusCode = 200;
-            token.saveUserToken(jsonToken, function (name) {
-                console.log(name);
-                res.write('<p>User Name: ' + jsonToken.name + '</p>' +
-                    '<p>User Id: ' + jsonToken.id.substring(0,4) + '******</p>' +
-                    '<p>Access Token: ' + '******' + '</p>' +
-                    '<p>Access Secret: ' + '******' + '</p>' +
-                    '<p><a href="/twitter/send?user=' + jsonToken.name  + '">Click here to Send a Tweet!</a></p>');
-                return res.end();
-            })
-        })
-    }
-    else if ('/twitter/send' === pureURL.pathname) {
-        queryData = querystring.parse(pureURL.query);
-        console.log(queryData);
-        token.getUserToken(queryData.user, function (token) {
-            console.log(token.username);
-            oauth.sendTweet(token.user_token, token.secret_token, function (data) {
-                console.log(data);
-                res.write('<p><a href="https://twitter.com/' + token.username + '">View the Tweet</a></p>');
-                return res.end();
-            })
-        })
-    }
-    else if ('/complete/github' === pureURL.pathname) {
-        queryData = querystring.parse(pureURL.query);
-        console.log(queryData);
-        token.verifyToken(queryData.state, function (result) {
-            if (result) {
-                oauth.getPlainToken(queryData.code, function (body) {
-                    const tokenData = querystring.parse(body);
-                    oauth.getGithubUserInfo(tokenData.access_token, function (body) {
-                        const replyJson = JSON.parse(body);
-                        res.write('<p>Page: 0z2530Q</p>' +
-                                  '<p>User: ' + replyJson.login + '</p>' +
-                                  '<p>Id: ' + replyJson.id + '</p>' +
-                                  // '<p>Access Token: ' + tokenData.access_token + '</p>' +
-                                  // '<p>Token Type: ' + tokenData.token_type + '</p>');
-                                  '<p>Access Token: ' + '******' + '</p>' +
-                                  '<p>Token Type: ' + '******' + '</p>');
-                        return res.end();
-                    });
-                });
-            }
-            else {
-                res.write('<h1>Token Broken...</h1>');
-                return res.end();
-            }
-        });
-    }
-    else if ('/test' === pureURL.pathname) {
-        // const result = pureURL.searchParams('key');
-        queryData = querystring.parse(pureURL.query);
-        console.log(queryData);
-        res.statusCode = 200;
-        res.write('<p>Key Value: '+ queryData.key +'</p>');
-        return res.end();
-    }
-    else {
-        res.write('<p>Bad location</p>');
-        return res.end();
-    }
+//allow custom header and CORS
+app.all('*', function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
+  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+  if (req.method == 'OPTIONS') {
+    res.sendStatus(204);
+  }
+  else {
+    next();
+  }
 });
 
-server.listen(8000);
+app.get('/', function (req, res) {
+  return res.json({code: 0, msg: 'home page'});
+});
+
+app.get('/login/verify', function(req, res) {
+  var code = req.query.code;
+  var result = null;
+  if (!code) {
+    return res.json({code: 1, msg: 'verify code required'});
+  }
+  axios({
+    method: 'POST',
+    url: 'https://github.com/login/oauth/access_token',
+    data: {
+      // 需要带上这些信息去请求
+      client_id: config.GITHUB_CLIENT_ID,
+      client_secret: config.GITHUB_CLIENT_SECRET,
+      code: code
+    }
+  }).then(function(response) {
+    // 处理下返回
+    console.log(response.data);
+    result = querystring.parse(response.data);
+    console.log(result);
+    // 没有错误发生， 成功
+    if (typeof(result.error) === "undefined") {
+      return res.json({code: 0});
+    // 失败的
+    } else {
+      return res.json({code: 1, msg: result.error});
+    }
+  }).catch(function(error) {
+    console.log(error);
+    res.json({code: 2});
+  });
+})
+
+http.listen(config.SERVER_PORT, function () {
+  console.log(`Server listening on ${config.SERVER_PORT}`);
+});
+
+// var server = http.createServer(function (req, res) {
+//   // Allow CORS
+//   res.setHeader('Access-Control-Allow-Origin', '*');
+//   res.setHeader('Access-Control-Allow-Credentials', 'true');
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, HEAD, OPTIONS, POST, PUT');
+//   res.setHeader('Access-Control-Allow-Headers', '*');
+//    if ( req.method === 'OPTIONS' ) {
+//     res.writeHead(204);
+//     res.end();
+//     return;
+//   }
+
+//   const pureURL = url.parse(req.url);
+//   console.log(pureURL.pathname);
+//   var queryData = {};
+//   if ('/' === pureURL.pathname) {
+//     res.statusCode = 200;
+//     res.write('<p><a href="/">Nothing</a></p>' +
+//               '<p><a href="/">Nothing</a></p>');
+//     return res.end();
+//   }
+//   // 验证github登录的code
+//   else if ('/login/verify' === pureURL.pathname) {
+//     queryData = querystring.parse(pureURL.query);
+//     let result = null;
+//     console.log(queryData);
+//     axios({
+//       method: 'POST',
+//       url: 'https://github.com/login/oauth/access_token',
+//       data: {
+//         // 需要带上这些信息去请求
+//         client_id: config.GITHUB_CLIENT_ID,
+//         client_secret: config.GITHUB_CLIENT_SECRET,
+//         code: queryData.code
+//       }
+//     }).then(function(response) {
+//       // 处理下返回
+//       console.log(response.data);
+//       result = querystring.parse(response.data);
+//       console.log(result);
+//       // 没有错误发生， 成功
+//       if (typeof(result.error) === "undefined") {
+//         res.write(`{code: 0}`);
+//         return res.end();
+//       // 失败的
+//       } else {
+//         res.write(`{code: 1, msg: ${result.error}}`);
+//         return res.end();
+//       }
+//     }).catch(function(error) {
+//       console.log(error);
+//       res.write(`{code: 2}`);
+//       return res.end();
+//     });
+//   }
+//   else {
+//     res.write('<p>Bad location</p>');
+//     return res.end();
+//   }
+// });
+
+// server.listen(8000);
