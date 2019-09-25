f2e99daa90b10b0b41c9037065aaf1ad471082e2
lychees <lychees67@gmail.com>
2019-09-26 02:47:41 +0800

    Go

--- contracts/Ethereum/migrations/2_deploy_contracts.js
+++ contracts/Ethereum/migrations/2_deploy_contracts.js
@@ -1,8 +1,5 @@
-const ConvertLib = artifacts.require("ConvertLib");
-const MetaCoin = artifacts.require("MetaCoin");
+const Advertisement2 = artifacts.require("Advertisement");
 
 module.exports = function(deployer) {
-  deployer.deploy(ConvertLib);
-  deployer.link(ConvertLib, MetaCoin);
-  deployer.deploy(MetaCoin);
+  deployer.deploy(Advertisement2, "0xE8B21A66d89401254045bAb95B474B52B6faC351", "0xE8B21A66d89401254045bAb95B474B52B6faC351");  
 };
