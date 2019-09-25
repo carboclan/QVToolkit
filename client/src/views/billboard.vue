28db242fef92ba4b69c957ada6034ad857789b3e
lychees <lychees67@gmail.com>
2019-09-26 04:54:23 +0800

    Go

---
+++ client/src/views/billboard.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="about">
+    <el-button type="text">buy</el-button>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+    };
+  },
+  methods: {
+    handleChange(value) {
+    },
+    async loadInfo() {
+    }
+  },
+  created: function() {
+    this.loadInfo();
+  }
+};
+</script> 
