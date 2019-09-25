9dfbe3c57b83dab0b92104e4a45da8d848a2f5f8
qinxiaowen <453000697@qq.com>
2019-09-25 19:53:32 +0800

    get github resource

--- client/src/views/About.vue
+++ client/src/views/About.vue
@@ -1,55 +1,67 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
-  <el-table
-    :data="tableData"
-    stripe
-    style="width: 100%">
-    <el-table-column
-      prop="title"
-      label="title"
-      width="180">
-    </el-table-column>
-    <el-table-column
-      prop="speaker"
-      label="speaker"
-      width="180">
-    </el-table-column>
-    <el-table-column
-      prop="period"
-      label="period">
-    </el-table-column>
-    <el-table-column
-      prop="vote"
-      label="vote">
-    </el-table-column>    
-  </el-table>
-      <el-input-number v-model="num" controls-position="right" @change="handleChange" :min="1" :max="10">233</el-input-number>
+    <el-table :data="tableData" stripe style="width: 100%">
+      <el-table-column prop="title" label="title" width="180"></el-table-column>
+      <el-table-column prop="speaker" label="speaker" width="180"></el-table-column>
+      <el-table-column prop="period" label="period"></el-table-column>
+      <el-table-column prop="vote" label="vote"></el-table-column>
+    </el-table>
+    <el-input-number
+      v-model="num"
+      controls-position="right"
+      @change="handleChange"
+      :min="1"
+      :max="10"
+    >233</el-input-number>
   </div>
- 
-
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        num: 1,
-        tableData: [{
-          title: 'Defi blabla',
-          speaker: 'Bowen',
-          period: '45min',
-        }, {
-          title: 'Ethereum 2.0 Deliver time table',
-          speaker: 'Vitalik',
-          period: '10min'
-        }]
-      }
-    },    
-    methods: {
-      handleChange(value) {
-        console.log(value);
-      }
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      num: 1,
+      tableData: [
+        {
+          title: "Defi blabla",
+          speaker: "Bowen",
+          period: "45min"
+        },
+        {
+          title: "Ethereum 2.0 Deliver time table",
+          speaker: "Vitalik",
+          period: "10min"
+        }
+      ]
+    };
+  },
+  methods: {
+    handleChange(value) {
+      console.log(value);
+    },
+    async loadInfo() {
+      axios
+        .get(
+          "https://raw.githubusercontent.com/carboclan/QVToolkit/master/client/package.json"
+        )
+        .then(response => {
+          let responseData = response.data;
+          this.tableData.push({
+            title: responseData.name,
+            speaker: responseData.scripts.build,
+            period: responseData.scripts.serve
+          });
+        })
+        .catch(error => {
+          this.errors.push(error);
+        });
     }
-  };
+  },
+  created: function() {
+    this.loadInfo();
+  }
+};
 </script> 
