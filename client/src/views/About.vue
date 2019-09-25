e50605289b2f5872fde95b114a722abc1acb840a
xiaotiandada <952822399@qq.com>
2019-09-25 19:24:14 +0800

    add vote table

--- client/src/views/About.vue
+++ client/src/views/About.vue
@@ -1,5 +1,65 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <el-table :data="tableData" style="width: 100%">
+      <el-table-column prop="title" label="title"> </el-table-column>
+      <el-table-column prop="speaker" label="speaker"> </el-table-column>
+      <el-table-column prop="period" label="period"> </el-table-column>
+      <el-table-column prop="vote" label="vote">
+        <template slot-scope="scope">
+          <el-input-number
+            v-model="scope.row.vote"
+            @change="handleChange"
+            :min="1"
+            :max="10"
+            label="vote"
+          ></el-input-number>
+        </template>
+      </el-table-column>
+    </el-table>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      tableData: [
+        {
+          title: "Defi blabla",
+          speaker: "Bowen",
+          period: "45min",
+          vote: 1
+        },
+        {
+          title: "Defi blabla",
+          speaker: "Bowen",
+          period: "45min",
+          vote: 1
+        },
+        {
+          title: "Defi blabla",
+          speaker: "Bowen",
+          period: "45min",
+          vote: 1
+        },
+        {
+          title: "Defi blabla",
+          speaker: "Bowen",
+          period: "45min",
+          vote: 1
+        }
+      ]
+    };
+  },
+  methods: {
+    handleChange(value) {
+      console.log(value);
+      console.log(this.tableData[0].vote);
+      console.log(this.tableData[1].vote);
+      console.log(this.tableData[2].vote);
+      console.log(this.tableData[3].vote);
+    }
+  }
+};
+</script>
