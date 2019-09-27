972940062b2eecf45694e050e818c7d75999c743
lychees <lychees67@gmail.com>
2019-09-27 16:16:18 +0800

    Go

--- contracts/Ethereum/migrations/2_deploy_contracts.js
+++ contracts/Ethereum/migrations/2_deploy_contracts.js
@@ -1,6 +1,7 @@
 /* globals artifacts */
 var ArtSteward = artifacts.require("./ArtSteward.sol");
 var Artwork = artifacts.require("./ERC721Full.sol");
+var BillBoard = artifacts.require("./BillBoard.sol");
 
 const deploymentAccount =  '0xE8B21A66d89401254045bAb95B474B52B6faC351'; //[0] address from mnemonic
 const artistAccount = '0xE8B21A66d89401254045bAb95B474B52B6faC351'; // artist account [on mainnet & rinkeby]
@@ -10,7 +11,7 @@ module.exports = function(deployer, network, accounts) {
     // deploy with mnemonic provider
     deployer.deploy(Artwork, "This Artwork Is Always OnSale", "TAIAOS").then((deployedArtwork) => {
       console.log(deployedArtwork.address);
-      return deployer.deploy(ArtSteward, artistAccount, deployedArtwork.address);
+      return deployer.deploy(BillBoard, artistAccount, deployedArtwork.address);
     });
   } else {
     // development deploy
